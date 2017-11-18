851248af81bda676811bb36d12d6901fda963cc9
Hans Lossmann <hanslossman@gmail.com>
2017-11-18 16:40:36 +0000

    My python projects

---
+++ projec1.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+
+print('hello Kaiwen')
+
+print('Here is a picture of a dog:')
+print ('O-----')
+print('  ||||')
---
+++ project2.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+#-*- coding: utf-8 -*-
+print"räksmörgås" 
+print ('Vi har en katt som heter Doodle.Han ser ut så har:')
+print ('<^--------/')
+print ('   |-----|')
+print ('   -     - ')
+print ('Jag bor i Lund')
+
+#version2
+
+print ('Vi har en katt som heter Doodle.Han ser ut så har:')
+
+print ('''
+<^--------/
+  |-----|
+''')
+
+print(2017 -2006)
+
+born=input('Vilket år är du född?')
+born=int(born)
+age=2017-born
+print('You age  is', age,'is i right?') 
